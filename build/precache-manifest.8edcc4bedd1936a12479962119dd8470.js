0957349f3e73578d839e783d64802371d5c86f09
David Loesch <davidloesch1@gmail.com>
2020-05-12 13:09:42 -0700

    adding new resume:

---
+++ build/precache-manifest.8edcc4bedd1936a12479962119dd8470.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e316b3208ba5195ac93767ed82074ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a4a0f7539f879c62c9",
+    "url": "/static/css/main.5d5b2ed5.chunk.css"
+  },
+  {
+    "revision": "eb1d275a393d7aea23d7",
+    "url": "/static/js/2.9d6bbe9f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9d6bbe9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a4a0f7539f879c62c9",
+    "url": "/static/js/main.88a2178e.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "2dfbdcd50d1ea4d4426a322ce9eccd7a",
+    "url": "/static/media/David_Loesch_Resume.2dfbdcd5.pdf"
+  },
+  {
+    "revision": "d1355cd35da5c20a2ca7086cf8092610",
+    "url": "/static/media/calendar.d1355cd3.png"
+  },
+  {
+    "revision": "9c2d2e9391260743ecc55019f9e13956",
+    "url": "/static/media/profile.9c2d2e93.jpg"
+  },
+  {
+    "revision": "7c0aa7ce34ece92705f5ca74b3bf47df",
+    "url": "/static/media/simon.7c0aa7ce.png"
+  },
+  {
+    "revision": "ad3eaea8ea505d81061fc8c694bc0943",
+    "url": "/static/media/storyboard.ad3eaea8.png"
+  },
+  {
+    "revision": "8fc3ba008b1a9d074f62126e5774d87b",
+    "url": "/static/media/todo_img.8fc3ba00.png"
+  }
+]);
